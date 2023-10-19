34f3445a378cb98431246f4e5d7556a1f796ba6e
Rebecca Turner <rbt@sent.as>
2023-10-18 18:36:44 -0700

    yeah

---
+++ src/coverage.rs
@@ -0,0 +1,85 @@
+use std::collections::BTreeMap;
+
+use camino::Utf8PathBuf;
+use lcov::Record;
+use miette::miette;
+use miette::IntoDiagnostic;
+
+#[derive(Debug)]
+pub struct Coverage {
+    state: State,
+    pub files: BTreeMap<Utf8PathBuf, LineData>,
+}
+
+#[derive(Debug)]
+enum State {
+    None,
+    SourceFile { path: Utf8PathBuf, data: LineData },
+}
+
+impl Coverage {
+    pub fn new() -> Self {
+        Self {
+            state: State::None,
+            files: Default::default(),
+        }
+    }
+
+    pub fn consume(&mut self, record: Record) -> miette::Result<()> {
+        match &mut self.state {
+            State::None => match record {
+                Record::SourceFile { path } => {
+                    self.state = State::SourceFile {
+                        path: path.try_into().into_diagnostic()?,
+                        data: LineData::default(),
+                    }
+                }
+                _ => {
+                    return Err(miette!("Unexpected record: {record:?}"));
+                }
+            },
+            State::SourceFile { path, data } => match record {
+                Record::LineData {
+                    line,
+                    count,
+                    checksum: _,
+                } => {
+                    *data.counts.entry(line).or_default() += count;
+                }
+                Record::LinesFound { found } => {
+                    data.found = Some(found);
+                }
+                Record::LinesHit { hit } => {
+                    data.hit = Some(hit);
+                }
+                Record::EndOfRecord => {
+                    let data = std::mem::take(data);
+                    self.files.insert(path.clone(), data);
+                    self.state = State::None;
+                }
+                _ => {
+                    return Err(miette!("Unexpected record: {record:?}"));
+                }
+            },
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct LineData {
+    hit: Option<u32>,
+    found: Option<u32>,
+    /// Map from line numbers to counts.
+    counts: BTreeMap<u32, u64>,
+}
+
+impl LineData {
+    pub fn coverage_fraction(&self) -> Option<f64> {
+        match (self.hit, self.found) {
+            (Some(hit), Some(found)) => Some((hit as f64) / (found as f64)),
+            _ => None,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
 use lcov::Reader;
 use miette::IntoDiagnostic;
 
+mod coverage;
+use coverage::Coverage;
+
 fn main() -> miette::Result<()> {
+    let mut coverage = Coverage::new();
     let reader = Reader::open_file("../broot.nvim/target/coverage.lcov").into_diagnostic()?;
-    let records = reader.collect::<Result<Vec<_>, _>>().into_diagnostic()?;
-    let mut maybe_hit = None;
-    let mut maybe_found = None;
-    for record in records {
-        match record {
-            lcov::Record::LinesFound { found } => {
-                maybe_found = Some(found);
-            }
-            lcov::Record::LinesHit { hit } => {
-                maybe_hit = Some(hit);
-            }
-            lcov::Record::EndOfRecord => {
-                if let Some(hit) = maybe_hit {
-                    if let Some(found) = maybe_found {
-                        println!("Coverage: {}", (hit as f64) / (found as f64));
-                    }
-                }
-                maybe_hit = None;
-                maybe_found = None;
-            }
-            _ => {}
-        }
-        println!("{:?}", record);
+    for item in reader {
+        let item = item.into_diagnostic()?;
+        coverage.consume(item)?;
+    }
+
+    for (file, data) in coverage.files.iter() {
+        println!("{file}: {:?}", data.coverage_fraction());
     }
 
     Ok(())
